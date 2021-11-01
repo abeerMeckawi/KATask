1a7889cc92eeb83ca6279c808aa1dea9fc2621b1
Abeer Meckawi <abeer.meckawii@gmail.com>
2021-11-01 09:12:02 -0700

    KATask

--- KATask/ViewController.swift
+++ KATask/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import ArcGIS
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AGSGeoViewTouchDelegate{
+    
+    @IBOutlet weak var viewMap : AGSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMap()
     }
-
-
+    
+    private func setupMap(){
+        let map = AGSMap(basemap: .navigationVector())
+        
+        viewMap.map = map
+        viewMap.touchDelegate = self
+        
+        viewMap.setViewpoint(
+            AGSViewpoint(
+                latitude: 34.02700,
+                longitude: -118.80500,
+                scale: 72_000
+            )
+        )
+    }
+    
+    func geoView(_ geoView: AGSGeoView, didTapAtScreenPoint screenPoint: CGPoint, mapPoint: AGSPoint) {
+        
+        if self.viewMap.callout.isHidden {
+            viewMap.callout.title = "Name"
+            viewMap.callout.detail = "Abeer"
+            viewMap.callout.isAccessoryButtonHidden = true
+            viewMap.callout.show(at: mapPoint, screenOffset: CGPoint.zero, rotateOffsetWithMap: false, animated: true)
+        } else {
+            viewMap.callout.dismiss()
+        }
+        
+    }
+    
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'KATask' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for KATask
+   pod 'ArcGIS-Runtime-SDK-iOS'
+
+end
